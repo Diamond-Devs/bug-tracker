9cc8f6f3318b95e16a2a852c6768ab681e6b0dbb
Riley Lynch <rileydlynch@yahoo.com>
2021-09-22 00:06:53 -0400

    Update app.js
    Added env variables for the mysql database and CORS-allowed domains

--- server/app.js
+++ server/app.js
@@ -12,14 +12,18 @@ app.use(express.static('public'));
 var connection = mysql.createConnection({ //This command is used to log into the database
   host     : 'localhost',
   user     : 'root',     // Insert your MySQL root username
-  database : 'dbname',   // Insert the name of your db
+  database : process.env.database,   // Insert the name of your db
 });
 
 app.use(function(req, res, next) {//This section allows specified foreign domains to access this node app
     console.log(`Domain origin of current request is: ${req.headers.origin}`);
+    if (req.headers.origin == undefined) {
+        console.log(req.headers)
+    }
+    
     const corsWhitelist = [//This identifies the exclusive domains allowed to access this app
-        'https://exampledomain1.com',
-        'https://exampledomain2.com'
+        process.env.corsdomain1,
+        process.env.corsdomain2
     ];
     //The below if statement sets CORS to allow many types of requests to be made to this app
     if (corsWhitelist.find(origin => origin == String(req.headers.origin))) {
@@ -31,6 +35,15 @@ app.use(function(req, res, next) {//This section allows specified foreign domain
 });
 
 app.post("/",(req,res,err) => {//Currently this is just confirming the front-end can send data to this node app
+    console.log(req.body)
+    if (err) {
+      console.log(err)
+    }else {
+        res.send("Good job, you logged in correctly")
+    }
+})
+
+app.post("login-route",(req,res,err) => {
     console.log(req.body)
     if (err) {
       console.log(err)
