b14396d8f85e983d10cfaf3769ff36f199d01643
Riley Lynch <rileydlynch@yahoo.com>
2021-09-21 21:42:12 -0400

    Update app.js
    Set CORS headers so now this node app can accept requests from specified foreign domains.

--- server/app.js
+++ server/app.js
@@ -2,18 +2,43 @@ var express = require('express');
 var app = express();
 var mysql = require('mysql2');
 var JSON = require('JSON');
+var cors = require('cors');
 
 app.set('view engine', 'html');
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(express.static('public'));
 
-var connection = mysql.createConnection({ //Use this command to log into the database manually: 'mysql -u root'
+var connection = mysql.createConnection({ //This command is used to log into the database
   host     : 'localhost',
-  user     : 'root',     // your root username
-  database : 'bugtracker',   // the name of your db
+  user     : 'root',     // Insert your MySQL root username
+  database : 'dbname',   // Insert the name of your db
 });
 
-app.listen(3000, function () {
- console.log('App listening on port 3000!');
+app.use(function(req, res, next) {//This section allows specified foreign domains to access this node app
+    console.log(req.headers.origin);
+    const corsWhitelist = [//This identifies the exclusive domains allowed to access this app
+        'https://exampledomain1.codeanyapp.com',
+        'https://exampledomain2.codeanyapp.com'
+    ];
+    //The below if statement sets CORS to allow many types of requests to be made to this app
+    if (corsWhitelist.find(origin => origin == String(req.headers.origin))) {
+        res.header("Access-Control-Allow-Origin","*");
+        res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    }
+    next();
+});
+
+app.post("/",(req,res,err) => {//Currently this is just confirming the front-end can send data to this node app
+    console.log(req.body)
+    if (err) {
+      console.log(err)
+    }
+})
+
+const port = 3001;
+
+app.listen(port, function () {
+ console.log(`App listening on port ${port}!`);
 });
