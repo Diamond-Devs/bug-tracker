0ac9af1c9203575334837cdc812b52966a821148
Riley Lynch <rileydlynch@yahoo.com>
2021-09-23 01:34:18 -0400

    Update app.js
    Added login route that checks username and password against the MySQL database using bcrypt.

--- server/app.js
+++ server/app.js
@@ -1,17 +1,21 @@
-var express = require('express');
-var app = express();
-var mysql = require('mysql2');
-var JSON = require('JSON');
-var cors = require('cors');
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+const JSON = require('JSON');
+const cors = require('cors');
+const bcrypt = require("bcrypt");
+const cookieParser = require('cookie-parser')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config({ path: __dirname + '/.env' });
 }
+
 console.log("Process started at: " + new Date());
 console.log("If anything shows up after this, the .env file has been loaded properly: " + process.env.corsdomain1);
 
 app.set('view engine', 'html');
+app.use(cookieParser());
 app.use(express.json()); //Used to parse JSON bodies
-app.use(express.urlencoded()); //Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
 app.use(express.static('public'));
 
 var connection = mysql.createConnection({ //This command is used to log into the database
@@ -39,21 +43,43 @@ app.use(function(req, res, next) {//This section allows specified foreign domain
     next();
 });
 
-app.post("/",(req,res,err) => {//Currently this is just confirming the front-end can send data to this node app
+app.post("/login-route", async function(req,res) {
     console.log(req.body)
-    if (err) {
-      console.log(err)
-    }else {
-        res.send("Good job, you logged in correctly")
-    }
-})
-
-app.post("login-route",(req,res,err) => {
-    console.log(req.body)
-    if (err) {
-      console.log(err)
-    }
-})
+	const Postres = res;
+    const Postreq = req;
+    let returndata = {};
+    var sqlstatement = "SELECT password FROM users WHERE username in ('" + req.body.name + "')"
+    connection.query(sqlstatement, async function(err, res) {
+		if(err){//This throws if the username doesn't exist in the database
+            console.log(err);
+            returndata.usernamepass = false;
+        }
+        else{
+            returndata.usernamepass = true;
+        }
+		ExistPW = res[0].password;
+		bcrypt.compare(Postreq.body.password, ExistPW, function(err, res) { //This function takes in the raw password (DO NO HASH IT), hashes it, then compares vs the database's hashed version of the password. DO NOT HASH THE PASSWORD BEFORE PASSING IT TO THE .compare METHOD!!
+			if (err){
+                console.log(err);
+                    returndata.usernamepass = false;
+                    returndata.passwordpass = false;
+                    Postres.json(returndata);
+			  }
+			if (res){
+                console.log("It's a match!");
+                returndata.passwordpass = true;
+                Postres.json(returndata);
+				Postres.cookie('userprofile', Postreq.body.username);
+				Postres.redirect("/Dashboard.js");
+			  }
+			else {
+                console.log("Not a match!");
+                returndata.passwordpass = false;
+                Postres.json(returndata);
+			  }
+		});
+	});
+});
 
 const port = 3001;
 
